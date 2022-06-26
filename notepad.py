42b0a9acd83f1978314d5af48cff702661aa3829
globeweis86 <globeweis86@email.com>
2022-06-26 10:29:32 -0600

    added notepad chages

--- notepad.py
+++ notepad.py
@@ -1,3 +1,5 @@
+"""notepad application"""
+
 import tkinter as tk
 import hashlib
 from pathlib import Path
@@ -9,15 +11,21 @@ root = tk.Tk()
 root.title('My Notepad')
 root.geometry('800x600')
 root.columnconfigure(0, weight=1)
-root.rowconfigure(2, weight=1)
+root.rowconfigure(3, weight=1)
 
 # subject
 
 subj_frame = tk.Frame(root)
 subj_frame.columnconfigure(1, weight=1)
 subject_var = tk.StringVar()
-tk.Label(subj_frame, text='Subject: ').grid(sticky='we', padx=5, pady=5)
-tk.Entry(subj_frame, textvariable=subject_var).grid(row=0, column=1, sticky=tk.E + tk.W)
+tk.Label(
+    subj_frame,
+    text='Subject: '
+).grid(sticky='we', padx=5, pady=5)
+tk.Entry(
+    subj_frame,
+    textvariable=subject_var
+).grid(row=0, column=1, sticky=tk.E + tk.W)
 subj_frame.grid(sticky='ew')
 
 # category
@@ -26,16 +34,25 @@ cat_frame = tk.Frame(root)
 cat_frame.columnconfigure(1, weight=1)
 cat_var = tk.StringVar()
 categories = ['Work', 'Hobbies', 'Health', 'Money']
-tk.Label(cat_frame, text='Category: ').grid(sticky=tk.E + tk.W, padx=5, pady=5)
-cat_inp = tk.OptionMenu(cat_frame, cat_var, *categories)
-cat_inp.grid(row=0, column=1, sticky=tk.E + tk.W, padx=5, pady=5)
+tk.Label(
+    cat_frame,
+    text='Category: '
+).grid(sticky=tk.E + tk.W, padx=5, pady=5)
+cat_inp = tk.OptionMenu(
+    cat_frame,
+    cat_var,
+    *categories
+).grid(row=0, column=1, sticky=tk.E + tk.W, padx=5, pady=5)
 cat_frame.grid(sticky='ew')
 
 # private
 
 private_var = tk.BooleanVar(value=False)
-private_inp = tk.Checkbutton(root, variable=private_var, text='Private')
-private_inp.grid(row=2, column=0, ipadx=5, ipady=5)
+private_inp = tk.Checkbutton(
+    root,
+    variable=private_var,
+    text='Private'
+).grid(row=2, column=0, ipadx=5, ipady=5)
 
 # message
 message_frame = tk.LabelFrame(root, text='Message')
@@ -46,21 +63,44 @@ message_inp.grid(sticky='nesw')
 scrollbar = tk.Scrollbar(message_frame)
 scrollbar.grid(row=0, column=1, sticky='nse')
 message_frame.grid(sticky='nsew')
-
 scrollbar.configure(command=message_inp.yview)
+message_inp.configure(yscrollcommand=scrollbar.set)
 
 # save button
 
-save_btn = tk.Button(root, text='save')
-save_btn.grid(row=99, column=1, sticky=tk.E, ipadx=5, ipady=5)
+save_btn = tk.Button(
+    root,
+    text='Save'
+)
+save_btn.grid(sticky=tk.E, ipadx=5, ipady=5)
 
 # status bar
 
 status_var = tk.StringVar()
-status_bar = tk.Label(root, textvariable=status_var)
-status_bar.grid(row=100, column=0, columnspan=2, ipadx=5, ipady=5)
+status_bar = tk.Label(
+    root,
+    textvariable=status_var
+).grid(row=100, ipadx=5, ipady=5)
 
 # functions and bindings
+def weaksauce_encrypt(text, password):
+    """Weak encryption"""
+
+    offset = sum([ord(x) for x in password])
+    encoded = ''.join(
+        chr(min(ord(x) + offset, 2**20))
+        for x in text
+    )
+    return encoded
+
+def weaksuace_decrypt(text, password):
+    """Weak Decryption"""
+    offset = sum([ord(x) for x in password])
+    decoded = ''.join(
+        chr(max(ord(x) - offset, 0))
+        for x in text
+    )
+    return decoded
 
 def save():
     """Save the data to a file"""
@@ -69,12 +109,11 @@ def save():
     category = cat_var.get()
     private = private_var.get()
     message = message_inp.get('1.0', tk.END)
-
+    extension = 'txt' if not private else 'secret'
+    filename = f'{category} - {subject}.{extension}'
     if private:
-        message = hashlib.md5(message.encode()).hexdigest()
+        message = weaksauce_encrypt(message, 'password')
 
-
-    filename = f'{category} - {subject}.txt'
     with open(filename, 'w') as fh:
         fh.write(message)
 
@@ -82,9 +121,23 @@ def save():
 
 save_btn.configure(command=save)
 
+def check_filename(*args):
+    """check if filename is already in use"""
+    subject = subject_var.get()
+    category = cat_var.get()
+    private = private_var.get()
 
+    extension = 'txt' if not private else 'secret'
+    filename = f'{category} - {subject}.{extension}'
 
+    if Path(filename).exists():
+        status_var.set(f'WARNING: {filename} already exists!')
+    else:
+        status_var.set('')
 
+subject_var.trace_add('write', check_filename)
+cat_var.trace_add('write', check_filename)
+private_var.trace_add('write', check_filename)
 
 # last line
 root.mainloop()
